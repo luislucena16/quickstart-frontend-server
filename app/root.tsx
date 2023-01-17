d9776649e7f934ffb61edffde9b69433d105a2ba
Luis Lucena <lucena@puller.mx>
2023-01-18 00:55:03 +0100

    adding route folders and styles

---
+++ app/root.tsx
@@ -0,0 +1,44 @@
+import type { LinksFunction, MetaFunction } from '@remix-run/node';
+import {
+  Links,
+  LiveReload,
+  Meta,
+  Outlet,
+  Scripts,
+  ScrollRestoration,
+} from '@remix-run/react';
+
+
+import tailwindStyles from '~/styles/tailwind.css';
+import vendorsStyles from '~/styles/vendors.css';
+
+export let links: LinksFunction = () => {
+  return [
+    { rel: 'stylesheet', href: 'https://rsms.me/inter/inter.css' },
+    { rel: 'stylesheet', href: vendorsStyles },
+    { rel: 'stylesheet', href: tailwindStyles },
+  ];
+};
+
+export const meta: MetaFunction = () => ({
+  charset: 'utf-8',
+  title: 'Welcome',
+  viewport: 'width=device-width,initial-scale=1',
+});
+
+export default function App() {
+  return (
+    <html lang="en">
+      <head>
+        <Meta />
+        <Links />
+      </head>
+      <body>
+        <Outlet />
+        <ScrollRestoration />
+        <Scripts />
+        <LiveReload />
+      </body>
+    </html>
+  );
+}
---
+++ app/routes/index.tsx
@@ -0,0 +1,34 @@
+export default function Index() {
+  return (
+    <div className="w-full mx-auto p-10">
+      <h1 className="text-5xl text-center font-bold">
+        Welcome
+      </h1>
+      <ul className="pt-5">
+        <li>
+          <a
+            target="_blank"
+            href="https://remix.run/tutorials/blog"
+            rel="noreferrer"
+          >
+            15m Quickstart Blog Tutorial
+          </a>
+        </li>
+        <li>
+          <a
+            target="_blank"
+            href="https://remix.run/tutorials/jokes"
+            rel="noreferrer"
+          >
+            Deep Dive Jokes App Tutorial
+          </a>
+        </li>
+        <li>
+          <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
+            Remix Docs
+          </a>
+        </li>
+      </ul>
+    </div>
+  );
+}
